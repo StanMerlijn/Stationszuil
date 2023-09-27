c18680dc6078f4cc5228959e10cc3b671b1fff9d
Stan Merlijn <stan.merlijn@student.hu.nl>
2023-09-27 11:29:30 +0200

    inputText.py added comments

--- inputText.py
+++ inputText.py
@@ -1,11 +1,10 @@
 import random
 from datetime import datetime
 
-yes_bool = "yesYesYy1"
+yes_bool = "yesYesYy1jaJa"
 
-
-# creating a function to get the current time and date.
 def get_time_date():
+    """this function retrieves the current time and date when called"""
     current_date = datetime.now().date()
     current_time = datetime.now().time().strftime("%X")
     return current_time, current_date
@@ -15,23 +14,25 @@ while True:
     randomNr = int(random.random())
     wants_write = input("do you want to write a message? ")
     time_now, date_now = get_time_date()
+    # checks if user wants to write a message. and if the user wants to input his/her name
     if wants_write in yes_bool:
         wantsName = input("Do you want to use your name(yes or no): ")
         if wantsName in yes_bool:
             name = input("What is your name: ")
         else:
             name = "anonymous"
-
+        # here the user inputs their message
         message = input("write your message here: ")
         with open("stations.txt", "r") as file:
             lines = file.readlines()
         random_line_number = random.randint(0, len(lines) - 1)
         station = lines[random_line_number].strip()
+        # checks if the message is bigger than 140 else it will ask to write the message again
         if len(message) > 140:
             print("your message should be less than 140 characters")
         else:
+            # writes all the given data in a txt file
             with open("text.txt", "a") as file:
                 file.write(f"{name}, {message}, {date_now}, {time_now}, {station}\n")
     else:
         break
-
