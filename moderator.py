d8af4fda87054094b185d1f1d8803db77c6dad84
Stan Merlijn <stan.merlijn@student.hu.nl>
2023-09-27 20:42:56 +0200

    moderator.py can connect to database

--- moderator.py
+++ moderator.py
@@ -21,21 +21,20 @@ def clear_file(input_file):
 
 with open("text.txt", "r+") as file:
     for line in file:
-        unapproved_text = line.strip().split(", ")
         name_user, message, date_now, time_now, station = line.strip().split(", ")
-        valid_text = input(f"Is this text by {name_user} valid: {unapproved_text[1]}: ")
-        clear_file(input_file_messages)
+        valid_text = input(f"Is this text by {name_user} valid: {message}: ")
+        listt = name_user, message, date_now, time_now, station
+        print(listt)
+
         # if moderator agrees that the text is valid it will be writen into the database
         if valid_text in yes_bool:
-            with open("approved_text.txt", "a") as write_file:
-                unapproved_text.append("approved")
-                write_file.write(f"{', '.join(unapproved_text)}\n")
             cur = connection.cursor()
+            cur.execute("INSERT INTO station (station_name) VALUES (%s)", (station,))
             insert_script = ("INSERT INTO ns_user (name_column, email_column, date_column,"
                              "time_column, message_column, station_name) "
-                             "VALUES (%s, %s, %s, %s, %s, %s, )")
-            insert_values = ("name_user", "email", date_now, time_now, "message", "station")
+                             "VALUES (%s, %s, %s, %s, %s, %s)")
+            insert_values = (name_user, email, date_now, time_now, message, station)
             cur.execute(insert_script, insert_values)
-            result = cur.fetchall()
             connection.close()
-            print(result)
+
+# clear_file(input_file_messages)
