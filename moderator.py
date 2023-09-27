9b609f019150f07336c3d687a99bf65365230804
Stan Merlijn <stan.merlijn@student.hu.nl>
2023-09-27 21:23:19 +0200

    moderator.py checks is station given is in DB already

--- moderator.py
+++ moderator.py
@@ -19,26 +19,33 @@ def clear_file(input_file):
         return
 
 
+def is_station_in_db(station_name):
+    """"boolean function to check if a station is already in the database"""
+    cursor = connection.cursor()
+    cursor.execute("SELECT COUNT(*) FROM station WHERE station_name = %s", (station_name,))
+    data = cursor.fetchone()[0]
+    cursor.close()
+    return data > 0
+
+
 with open("text.txt", "r+") as file:
     for line in file:
         name_user, message, date_now, time_now, station = line.strip().split(", ")
         valid_text = input(f"Is this text by {name_user} valid: {message}: ")
-        listt = name_user, message, date_now, time_now, station
-        print(listt)
-        print(name_user)
 
         # if moderator agrees that the text is valid it will be writen into the database
         if valid_text in yes_bool:
             cur = connection.cursor()
-            sql_query = "INSERT INTO station (station_name) VALUES (%s)"
-            cur.execute(sql_query, (station,))
+            # if the random station is already in the DB it will not write it to it
+            if is_station_in_db(station) is False:
+                sql_query = "INSERT INTO station (station_name) VALUES (%s)"
+                cur.execute(sql_query, (station,))
             insert_script = ("INSERT INTO ns_user (name_column, email_column, date_column,"
                              "time_column, message_column, station_name) "
                              "VALUES (%s, %s, %s, %s, %s, %s)")
             insert_values = (name_user, email, date_now, time_now, message, station)
             cur.execute(insert_script, insert_values)
             connection.commit()
-            cur.close()
-            connection.close()
-
-# clear_file(input_file_messages)
+    cur.close()
+    connection.close()
+clear_file(input_file_messages)
