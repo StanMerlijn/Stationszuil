eca36fd38926564b81652f9d6cd875a93d794c09
Stan Merlijn <stan.merlijn@student.hu.nl>
2023-10-04 22:27:27 +0200

    cleanup

--- moderator.py
+++ moderator.py
@@ -9,10 +9,19 @@ connection = psycopg2.connect(
     password="Whynow3421!"
 )
 
-yes_bool = "yesYesYy1jaJajJOKok"
 file_messages = "text.csv"
 
-email = input("before moderating could enter your email: ")
+
+def is_yes(input_text):
+    yes_bool = "yesYesYy1jaJajJOKok"
+    return input_text in yes_bool
+
+
+def file_is_empty():
+    with open(file_messages) as csv_file:
+        lines = csv_file.readlines()
+        return len(lines) == 0
+
 
 def get_time_date():
     date = datetime.now().date()
@@ -26,49 +35,50 @@ def clear_file(input_file):
         return
 
 
-def is_station_in_db(station_name):
+def is_station_in_db(cursor, station_name):
     """"boolean function to check if a station is already in the database"""
-    cursor = connection.cursor()
     cursor.execute("SELECT COUNT(*) FROM station WHERE station_name = %s", (station_name,))
     data = cursor.fetchone()[0]
-    cursor.close()
     return data > 0
 
 
-while True:
+if not file_is_empty():
+    email = input("before moderating could enter your email: ")
+
+
+def write_data(cursor, mod_email, line):
     current_time, current_date = get_time_date()
-    with open(file_messages, "r") as file:
-        cur = connection.cursor()
-        for line in file:
-            # extract data from the file
-            name_user, message, date_message, time_message, station = line.strip().split(", ")
+    name_user, message, date_message, time_message, station = line.strip().split(", ")
 
-            # prompt for validation
-            valid_text = input(f"Is this text by {name_user} valid: {message}: ")
+    # if moderator agrees that the text is valid it will be writen into the database
+    if is_yes(input(f"Is this text by {name_user} valid: {message}: ")):
 
-            # if moderator agrees that the text is valid it will be writen into the database
-            if valid_text in yes_bool:
-                email = input("moderator email: ")
+        # if the random station is already in the DB it will not write it to it
+        if is_station_in_db(cursor, station) is False:
+            cursor.execute("INSERT INTO station (station_name) VALUES (%s)", (station,))
 
-                # if the random station is already in the DB it will not write it to it
-                if is_station_in_db(station) is False:
-                    # insert station into station table
-                    sql_query = "INSERT INTO station (station_name) VALUES (%s)"
-                    cur.execute(sql_query, (station,))
+        # insert user data into the ns_user table
+        insert_script = ("INSERT INTO ns_user (name_column, mod_email, date_column,"
+                         "time_column, message_column, station_name, mod_date, mod_time) "
+                         "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)")
+        # The values that will be written to its respectable columns
+        insert_values = (name_user, mod_email, date_message, time_message,
+                         message, station, current_date, current_time)
+        cursor.execute(insert_script, insert_values)
+        return True
+    return False
 
-                # insert user data into the ns_user table
-                insert_script = ("INSERT INTO ns_user (name_column, email_column, date_column,"
-                                 "time_column, message_column, station_name, mod_date, mod_time) "
-                                 "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)")
 
-                # The values that will be written to its respectable columns
-                insert_values = (name_user, email, date_message, time_message,
-                                 message, station, current_date, current_time)
-                cur.execute(insert_script, insert_values)
-                connection.commit()
-            else:
-                print("There are no text to moderate")
-    cur.close()
+def write_to_db(filename):
+    cursor = connection.cursor()
+    while not file_is_empty():
+        with open(filename) as csv_file:
+            for line in csv_file:
+                if write_data(cursor, email, line):
+                    connection.commit()
+        clear_file(filename)
+    cursor.close()
     connection.close()
-    clear_file(file_messages)
-    break
+
+
+write_to_db(file_messages)
