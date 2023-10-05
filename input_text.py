0886634da5ecc857f445c4bd908cb2715f55e6e5
Stan Merlijn <stan.merlijn@student.hu.nl>
2023-10-05 02:46:57 +0200

    split code into 2 functions

--- input_text.py
+++ input_text.py
@@ -16,31 +16,35 @@ def get_time_date():
     return time, date
 
 
-def collect_write_message(output_file):
+def collect_user_input():
+    if is_yes(input("do you want to write a message? ")):
+        if is_yes(input("Do you want to use your name(yes or no): ")):
+            name = input("What is your name: ")
+        else:
+            name = "anonymous"
+        with open("stations.txt") as file:
+            stations = [line.strip() for line in file]
+        random_station = random.choice(stations)
+        # here the user inputs there message
+        message = input("write your message here: ")
+        return name, message, random_station
+    return False
+
+
+def write_data_to_file(output_file):
     while True:
         time_now, date_now = get_time_date()
-        if is_yes(input("do you want to write a message? ")):
-            if is_yes(input("Do you want to use your name(yes or no): ")):
-                name = input("What is your name: ")
-            else:
-                name = "anonymous"
-
-            with open("stations.txt") as file:
-                stations = [line.strip() for line in file]
-            random_station = random.choice(stations)
-
-            # here the user inputs there message
-            message = input("write your message here: ")
+        user_data = collect_user_input()
+        if not collect_user_input():
+            name, message, random_station = user_data
             if len(message) >= 140:
                 print("your message should be less than 140 characters")
             else:
                 with open(output_file, "a") as csv_file:
                     csv_file.write(f"{name}, {message}, {date_now}, {time_now}, {random_station}\n")
-            return True
-        return False
+                return True
+        else:
+            return False
 
 
-# def write_data_to_file(name, message, random_station, outputfile):
-
-
-print(collect_write_message(file_messages))
+write_data_to_file(file_messages)
