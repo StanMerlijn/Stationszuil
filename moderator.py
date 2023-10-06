9fd9f8d13dca29c18909cfde6302cc9226a40c88
Stan Merlijn <stan.merlijn@student.hu.nl>
2023-10-06 02:51:17 +0200

    will stop program when entering exit

--- moderator.py
+++ moderator.py
@@ -3,10 +3,10 @@ from datetime import datetime
 
 # connecting to the database
 connection = psycopg2.connect(
-    host="20.68.149.147",
-    database="NS_messages",
+    host="localhost",
+    database="NS messages",
     user="postgres",
-    password="wachtwoord"
+    password="Whynow3421!"
 )
 
 file_messages = "text.csv"
@@ -47,8 +47,9 @@ def initialize_data(cursor, mod_email, line):
     current_time, current_date = get_time_date()
     name_user, message, date_message, time_message, station = line.strip().split(", ")
 
+    user_input = input(f"Is this text by {name_user} valid: {message}: ")
     # if moderator agrees that the text is valid it will be writen into the database
-    if is_yes(input(f"Is this text by {name_user} valid: {message}: ")):
+    if is_yes(user_input):
 
         # if the random station is already in the DB it will not write it to it
         if is_station_in_db(cursor, station) is False:
@@ -62,23 +63,30 @@ def initialize_data(cursor, mod_email, line):
         insert_values = (name_user, date_message, time_message,
                          message, station, mod_email, current_date, current_time)
         cursor.execute(insert_script, insert_values)
-        return True
-    return False
+        return True, user_input
+    return False, user_input
 
 
 def write_data(cursor, filename, email):
-    while file_not_empty():
-        with open(filename) as csv_file:
+    exit_bool = False
+    with open(filename) as csv_file:
+        all_messages = csv_file.readlines()
+        print(all_messages)
+        while not exit_bool:
             for line in csv_file:
-                if initialize_data(cursor, email, line):
+                bool_approved, user_input = initialize_data(cursor, email, line)
+                if bool_approved:
                     connection.commit()
-        clear_file(filename)
-    else:
-        print("There is no data available to moderate")
+                    # all_messages.remove(f"{line}\\n")
+                    # print(all_messages)
+                elif user_input.lower() == "exit":
+                    exit_bool = True
+                    break
 
 
 def write_to_db(filename):
     if file_not_empty():
+
         email = input("before moderating could enter your email: ")
 
         cursor = connection.cursor()
