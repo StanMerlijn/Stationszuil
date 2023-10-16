30aca728739f2bd182b439c49139d0be91765bb0
Stan Merlijn <stan.merlijn@student.hu.nl>
2023-10-17 01:33:49 +0200

    input_text.py removed unused code.

--- input_text.py
+++ input_text.py
@@ -3,31 +3,24 @@ import psycopg2
 from datetime import datetime
 
 
-# Function to connect to the databasa
+# Function to connect to the database
 def connect_to_db():
     # Prompting for the database password
-    db_password = input("DB password: ")
+    while True:
+        try:
+            db_password = input("DB password: ")
 
-    # Establishing a connection to the PostgreSQL database
-    connection = psycopg2.connect(
-        host="20.68.149.147",
-        database="NS_messages",
-        user="postgres",
-        password=db_password
-    )
-    return connection
-
-
-# Function to clear a file
-def clear_file(filename):
-    with open(filename, "w") as file:
-        file.truncate()
-
-
-# Function to check if input corresponds to 'yes'
-def is_input_yes(input_text):
-    yes_bool = ["yes", "y", "ye"]
-    return input_text.lower() in yes_bool
+            # Establishing a connection to the PostgreSQL database
+            connection = psycopg2.connect(
+                host="20.68.149.147",
+                database="NS_messages",
+                user="postgres",
+                password=db_password
+            )
+            return connection
+        except psycopg2.OperationalError as error:
+            print("Error:", str(error))
+            print("Invalid password. Please try again.")
 
 
 # Function to get the current time and date
@@ -46,63 +39,11 @@ def prepare_message_data():
     return insert_script
 
 
-# Function to collect user input for writing messages
-def collect_user_input():
-    time_now, date_now = get_time_date()
-    print("-" * 50)
-
-    if is_input_yes(input(f"do you want to write a message (yes or no): ")):
-
-        if is_input_yes(input("Do you want to use your name (yes or no): ")):
-            name = input("What is your name: ")
-        else:
-            name = "anonymous"
-        with open("stations.txt") as file:
-            stations = [line.strip() for line in file]
-        # chooses a random station from file
-        random_station = random.choice(stations)
-        message = input("write your message here: ")
-
-        message_data = name, date_now, time_now, message, random_station
-        return True, message_data
-    return False, None
-
-
-# Function to write message data to a file
-def write_data_to_file(output_file):
-    while True:
-        is_valid, message_data = collect_user_input()
-        if is_valid:
-            name, date_now, time_now, message, random_station = message_data
-            if len(message) >= 140:
-                print("your message should be less than 140 characters")
-            else:
-                with open(output_file, "a") as csv_file:
-                    csv_file.write(f"{name}, {message}, {date_now}, {time_now}, {random_station}\n")
-        else:
-            break
-
-
 # Function to write message data to the database
 def write_data_to_db(cursor, message_data, connection):
     insert_script = prepare_message_data()
     cursor.execute(insert_script, message_data)
     connection.commit()
-
-
-# Main function
-def main(filename):
-    where_to_write = int(input("Write to DB else it will be file: "))
-    if where_to_write == 1:
-        with connect_to_db() as connection, connection.cursor() as cursor:
-            while True:
-                is_valid, message_data = collect_user_input()
-                if is_valid:
-                    write_data_to_db(cursor, message_data, connection)
-                else:
-                    break
-    else:
-        write_data_to_file(filename)
 
 
 def main_gui(cursor, connection, name, message):
@@ -122,4 +63,5 @@ def main_gui(cursor, connection, name, message):
 if __name__ == "__main__":
     file_messages = "text.csv"
     # main(file_messages)
-    main_gui("ass", "cheekcs")
+    # maingui()
+
