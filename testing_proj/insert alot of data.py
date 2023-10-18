c55415e039e47503eeb66ff6d943badfea695ab7
Stan Merlijn <stan.merlijn@student.hu.nl>
2023-10-18 16:30:45 +0200

    more testing non important

--- testing_proj/insert alot of data.py
+++ testing_proj/insert alot of data.py
@@ -1,21 +1,35 @@
+import random
+
 from input_text import get_time_date, write_data_to_db, get_random_station, connect_to_db
 import time
 
 
-def write_shit(cur, conn):
-    name = "anonymous"
+def generate_random_name():
+    length = random.randint(1, 10)  # Random length between 1 and 10
+    name = ''.join(chr(random.randint(97, 122)) for _ in range(length))
+    return name
+
+
+def write_shit(cur):
     start = time.time()
-    for i in range(1, 1001):
-        start = time.time()
+    station = "Oss"
+    name = "anonymous"
+    for i in range(1, 5001):
         message = i
         time_now, date_now = get_time_date()
-        random_station = get_random_station(cur)
-        message_data = name, date_now, time_now, message, random_station
-        write_data_to_db(cur, message_data, conn)
+        message_data = name, date_now, time_now, message, station
+        write_data_to_db(cur, message_data)
+
     end = time.time()
     print(f"time elapsed = {end - start}")
 
 
+def write_ass(cur, data):
+    write_data_to_db(cur, data)
+
+
 if __name__ == '__main__':
-    with connect_to_db() as conn, conn.cursor() as cursor:
-        write_shit(cursor, conn)
+    with (connect_to_db() as conn, conn.cursor() as cursor):
+
+        write_shit(cursor)
+        conn.commit()
--- testing_proj/testing code.py
+++ testing_proj/testing code.py
@@ -1,14 +1,16 @@
 import time
 from input_text import connect_to_db
+import timeit
+import sys
 
 
 def get_data_row(cursor):
     value = "anonymous"
     cursor.execute("SELECT * FROM message_send WHERE name_user = %s", (value,))
     rows = cursor.fetchone()
-    #filtered_rows = [row for row in rows if row != ('anonymous',)]
-    #print(filtered_rows)
-    print(rows)
+    # filtered_rows = [row for row in rows if row != ('anonymous',)]
+    # print(filtered_rows)
+    # print(rows)
 
 
 def get_data_all_rows(cursor):
@@ -16,23 +18,24 @@ def get_data_all_rows(cursor):
     rows = cursor.fetchall()
     # filtered_rows = [row for row in rows if row != ('anonymous',)]
     # print(filtered_rows)
-    print(rows)
+    # print(rows)
 
 
 def get_data_column(cursor):
     cursor.execute("SELECT name_user FROM message_send")
     rows = cursor.fetchall()
-    #filtered_rows = [row for row in rows if row != ('anonymous',)]
+    return rows
+    # filtered_rows = [row for row in rows if row != ('anonymous',)]
     # print(filtered_rows)
-    print(rows)
+    # print(rows)
 
 
 def get_data_columns(cursor):
     cursor.execute("SELECT name_user, date_message, time_message FROM message_send")
     rows = cursor.fetchall()
-    #filtered_rows = [row for row in rows if row != ('anonymous',)]
+    # filtered_rows = [row for row in rows if row != ('anonymous',)]
     # print(filtered_rows)
-    print(rows)
+    # print(rows)
 
 
 def get_time(func, cur):
@@ -43,11 +46,127 @@ def get_time(func, cur):
     return elapsed_time
 
 
+def is_user_id_unique(user_id, cursor):
+    cursor.execute("SELECT COUNT(*) FROM message_send WHERE name_user = %s", (user_id,))
+    count = cursor.fetchone()[0]
+    return count == 0
+
+
+def name_in_set(list, target_value):
+    list = set(list)
+    if target_value in list:
+        return True
+
+
+def binary_search(arr, target):
+    """
+    Perform a binary search on a sorted list.
+
+    Parameters:
+    arr (list): Sorted list of elements.
+    target: Value to search for.
+
+    Returns:
+    int: Index of the target value in the list. Returns -1 if not found.
+    """
+    start = 0
+    end = len(arr) - 1
+
+    while start <= end:
+        mid = (start + end) // 2
+        mid_val = arr[mid]
+
+        if mid_val == target:
+            return mid  # Target value found
+        elif mid_val < target:
+            start = mid + 1  # Search in the right half
+        else:
+            end = mid - 1  # Search in the left half
+
+    return -1  # Target value not found
+
+
+# Example usage
+target_value = 'stan'
+
+def time_set_sort(list, target_value):
+    time_taken = timeit.timeit(
+        stmt=lambda: name_in_set(list, target_value),
+        number=10000
+    )
+    print(f"Average time taken for 1.000 .000 runs: {time_taken:.5f} seconds")
+
+def time_binary_search(sorted_list, target_value):
+    # sorted_list = [i for i in range(100000)]  # Generate a sorted list
+    # target_value = 99999  # Target value to search for
+
+    # Time the binary_search function
+    time_taken = timeit.timeit(
+        stmt=lambda: binary_search(sorted_list, target_value),
+        number=100000
+    )
+    print(f"Average time taken for bin 1.000 .000 runs: {time_taken:.5f} seconds")
+
+
+def time_binary_search_set(sorted_list, target_value):
+    sorted_list = set(sorted_list)
+    sorted_list = list(sorted_list)
+    # Time the binary_search function
+    time_taken = timeit.timeit(
+        stmt=lambda: binary_search(sorted_list, target_value),
+        number=100000
+    )
+    print(f"Average time taken for bin set 1.000 .000 runs: {time_taken:.5f} seconds")
+
+
+
+def time_in_operation(names, target_name):
+
+
+    # Time the 'in' operation
+    time_taken = timeit.timeit(
+        stmt=lambda: target_name in names,
+        number=10000
+    )
+    print(f"Average time taken for 1.000 .000 runs: {time_taken:.10f} seconds")
+
+
+def time_is_user_id_unique(cursor, user_id):
+
+    # Time the is_user_id_unique function
+    time_taken = timeit.timeit(
+        stmt=lambda: is_user_id_unique(user_id, cursor),
+        number=1000  # Run the function 1000 times
+    )
+    print(f"Average time taken for 1.000 .000 runs: {time_taken:.10f} seconds")
+
+
 if __name__ == '__main__':
     with connect_to_db() as conn, conn.cursor() as cursor:
         print(f"all rows time = {get_time(get_data_all_rows, cursor)}")
         print(f"column time = {get_time(get_data_column, cursor)}")
         print(f"columns time= {get_time(get_data_columns, cursor)}")
         print(f"row time = {get_time(get_data_row, cursor)}")
+
+        names = []
+        rows = get_data_column(cursor)
+        for row in rows:
+            for name in row:
+                names.append(name)
+
+        size_list_names = sys.getsizeof(names)
+        element_size = sys.getsizeof(names[3])
+        total_size = size_list_names + len(names) * element_size
+
+        size_set_names = sys.getsizeof(set(names))
+        total_size_set = size_set_names + len(names) * element_size
+
+        print(f"\nsize of list names {total_size}\nsize of set names {total_size_set}")
+
+        #time_is_user_id_unique(cursor, "stan")
+        #time_in_operation(names, "stan")
+        time_binary_search(names, "stan")
+        time_binary_search_set(names, "stan")
+        #time_set_sort(names, "stan")
 
         conn.commit()
